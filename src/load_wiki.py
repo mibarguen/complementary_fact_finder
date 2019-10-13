c63756eac0cfe398e423ca899d62806778f59536
Mateo <mateoibarguen@gmail.com>
2019-10-12 17:11:57 -0700

    Tokenize sentences on WikiCorpus loader.

--- src/load_wiki.py
+++ src/load_wiki.py
@@ -1,23 +1,31 @@
 from gensim.corpora.wikicorpus import WikiCorpus
-
+from nltk.tokenize import sent_tokenize
 
 class LoadWiki:
     def __init__(self, link):
         self.link = link
-        self.wiki = WikiCorpus(self.link)
+        self.wiki = WikiCorpus(self.link, tokenizer_func=self.tokenize, processes=7)
+        print('here')
+        # self.wiki = WikiCorpus.load('data/wiki_model.dict')
+        self.wiki.save('data/wiki_model_sents.dict')
         self.num_docs = 0
         self.output_file = 'data/wiki_corpus.txt'
 
-    def load_text(self, t):
+    def load_text(self, text):
+        return ' '.join([t.decode("utf-8") for t in text])
+
+    def write_file(self, t, output_write):
+#        text = self.load_text(t)
+        output_write.write('\n'.join(t))
         self.num_docs += 1
         if self.num_docs % 100 == 0:
             print(f'{self.num_docs} processed. ')
-        return bytes(' '.join(t), 'utf-8').decode('utf-8') + '\n'
+        return t
 
-    def write_file(self, t, output_write):
-        text = self.load_text(t)
-        output_write.write(text)
-        return text
+    def tokenize(self, text, token_min_len, token_max_len, lower):
+        # override original method in wikicorpus.py
+        return [token for token in sent_tokenize(text)
+                if len(token) >= 20 and not token.startswith('_')]
 
     def load_corpus(self):
         output_write = open(self.output_file, 'w')
